3003eb94f10de85dd3013e0fa7bf0f10ccc11a1d
Navin Patwari <navin1@Navins-MacBook-Pro.local>
2025-06-08 11:41:40 +0530

    Intial Setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,10 @@
-#!/bin/bash
-find . -type f -exec sed -i 's/{dockerHubUsername}/'$1'/g' {} +
+#!/usr/bin/env bash
+if [[ $# -lt 1 ]]; then
+  echo "Usage: $0 <dockerHubUsername>"
+  exit 1
+fi
+
+USER="$1"
+
+# Use an empty string '' so BSD sed does in-place edits without backups
+find . -type f -exec sed -i '' "s/pnavin/${USER}/g" {} \;
